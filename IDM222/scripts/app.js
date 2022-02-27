0a48daf42881ad46ea6b3f0a8cc576220ca622e2
Prd46 <prd46@drexel.edu>
2022-02-26 20:42:08 -0500

    Navigation skeleton / basic index framework

---
+++ IDM222/scripts/app.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelector('.header__hamburger');
+menuButton.addEventListener('click', function(){
+const menu = document.querySelector('.header__menuListings'); 
+menu.classList.toggle('open');  
+})
+
+const rightArrow = document.querySelector('.rightArrow');
+const leftArrow = document.querySelector('.leftArrow');
